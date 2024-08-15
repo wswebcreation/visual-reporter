2d1b63e50da2c51ef5152b2af576af0d416d88e4
wswebcreation <wim@wswebcreation.nl>
2024-08-15 10:59:42 +0200

    feat: add filters

---
+++ src/app/components/SelectHeader.tsx
@@ -0,0 +1,206 @@
+"use client";
+
+import React from "react";
+import Select, {
+  components,
+  MultiValueGenericProps,
+  StylesConfig,
+} from "react-select";
+import styles from "./SelectHeader.module.css";
+import { SnapshotInstanceData } from "../types";
+
+interface OptionType {
+  label: string;
+  value: string;
+}
+const MAX_DISPLAYED_BADGES = 2;
+const MultiValueContainer = (
+  props: MultiValueGenericProps<OptionType, true>
+) => {
+  const { children, selectProps, data } = props;
+  const selectedValues = selectProps.value as OptionType[];
+
+  const index = selectedValues.findIndex(
+    (selectedOption) => selectedOption.value === data.value
+  );
+
+  if (index < MAX_DISPLAYED_BADGES) {
+    return (
+      <components.MultiValueContainer {...props}>
+        {children}
+      </components.MultiValueContainer>
+    );
+  }
+
+  if (index === MAX_DISPLAYED_BADGES) {
+    const remainingBadgeCount = selectedValues.length - MAX_DISPLAYED_BADGES;
+    return (
+      <div className={styles.overflowBadge}>+{remainingBadgeCount} more</div>
+    );
+  }
+
+  return null;
+};
+
+const SelectHeader = ({
+  handleSelectOptions,
+  instanceData,
+}: {
+  handleSelectOptions: (
+    selectedOptions: string[] | "all" | "passed" | "failed",
+    type: string
+  ) => void;
+  instanceData: SnapshotInstanceData;
+}) => {
+  const appOptions = instanceData?.app
+    ? instanceData.app.map((instance: string) => ({
+        value: instance,
+        label: instance,
+      }))
+    : [];
+  const browserOptions = instanceData?.browser
+    ? instanceData.browser.map(
+        (instance: { name: string; version: string }) => ({
+          value: `${instance.name}-${instance.version}`,
+          label: `${
+            instance.name.charAt(0).toUpperCase() + instance.name.slice(1)
+          } ${instance.version}`,
+        })
+      )
+    : [];
+  const deviceNameOptions = instanceData?.deviceName
+    ? instanceData.deviceName.map((instance: string) => ({
+        value: instance,
+        label: instance.charAt(0).toUpperCase() + instance.slice(1),
+      }))
+    : [];
+  const platformOptions = instanceData?.platform
+    ? instanceData.platform.map(
+        (instance: { name: string; version: string }) => ({
+          value: `${instance.name}-${instance.version}`,
+          label: `${
+            instance.name.charAt(0).toUpperCase() + instance.name.slice(1)
+          } ${instance.version}`,
+        })
+      )
+    : [];
+  const selectStyles: StylesConfig<OptionType, true> = {
+    control: (originalStyles) => ({
+      ...originalStyles,
+      marginLeft: "0.5rem",
+      borderColor: "white",
+      backgroundColor: "black",
+      width: "100%",
+    }),
+    menu: (originalStyles, {}) => ({
+      ...originalStyles,
+      backgroundColor: "black",
+      borderColor: "white",
+    }),
+    option: (originalStyles, state) => ({
+      ...originalStyles,
+      backgroundColor: state.isFocused
+        ? "transparent"
+        : state.isSelected
+        ? "white"
+        : originalStyles.backgroundColor,
+      color: state.isFocused
+        ? "inherit"
+        : state.isSelected
+        ? "black"
+        : originalStyles.color,
+      ":active": {
+        backgroundColor: "white",
+        color: "black",
+      },
+      ":hover": {
+        backgroundColor: "white",
+        color: "black",
+      },
+    }),
+    singleValue: (originalStyles) => ({
+      ...originalStyles,
+      color: "white",
+    }),
+  };
+  return (
+    <div className={styles.container}>
+      <Select
+        onChange={(selectedOption) =>
+          // @ts-ignore
+          handleSelectOptions(selectedOption.value, "status")
+        }
+        placeholder="Select the Status"
+        options={[
+          { value: "all", label: "All" },
+          { value: "passed", label: "Passed" },
+          { value: "failed", label: "Failed" },
+        ]}
+        styles={selectStyles}
+      />
+      {appOptions.length > 0 && (
+        <Select
+          components={{ MultiValueContainer }}
+          isMulti
+          onChange={(selectedOptions) =>
+            handleSelectOptions(
+              selectedOptions.map((option) => option.value),
+              "app"
+            )
+          }
+          placeholder="Select one or more apps"
+          options={appOptions}
+          styles={selectStyles}
+        />
+      )}
+      {browserOptions.length > 0 && (
+        <Select
+          components={{ MultiValueContainer }}
+          hideSelectedOptions={false}
+          isMulti
+          onChange={(selectedOptions) =>
+            handleSelectOptions(
+              selectedOptions.map((option) => option.value),
+              "browser"
+            )
+          }
+          placeholder="Select one or more browsers"
+          options={browserOptions}
+          styles={selectStyles}
+        />
+      )}
+      {deviceNameOptions.length > 0 && (
+        <Select
+          components={{ MultiValueContainer }}
+          isMulti
+          onChange={(selectedOptions) =>
+            handleSelectOptions(
+              selectedOptions.map((option) => option.value),
+              "device"
+            )
+          }
+          placeholder="Select one or more devices"
+          options={deviceNameOptions}
+          styles={selectStyles}
+        />
+      )}
+      {platformOptions.length > 0 && (
+        <Select
+          components={{ MultiValueContainer }}
+          isMulti
+          onChange={(selectedOptions) =>
+            handleSelectOptions(
+              selectedOptions.map((option) => option.value),
+              "platform"
+            )
+          }
+          placeholder="Select one or more platforms"
+          options={platformOptions}
+          styles={selectStyles}
+        />
+      )}
+    </div>
+  );
+};
+
+export default SelectHeader;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,99 @@
 "use client";
 
-import React from "react";
+import React, { useState, useMemo } from "react";
 import styles from "./page.module.css";
 import Description from "./components/Description";
 import GetSnapshotData from "./hooks/getSnapshotData";
+import SelectHeader from "./components/SelectHeader";
+import {
+  SnapshotInstanceData,
+  DescriptionData,
+  TestData,
+  MethodData,
+} from "./types";
 
 const Home: React.FC = () => {
   const { descriptionData, error, instanceData } = GetSnapshotData();
 
-  console.log("instanceData = ", instanceData);
+  const [selectedOptions, setSelectedOptions] = useState({
+    app: [] as string[],
+    browser: [] as string[],
+    device: [] as string[],
+    platform: [] as string[],
+    status: "all" as "all" | "passed" | "failed",
+  });
+
+  const handleSelectOptions = (
+    selectedOptions: string[] | "all" | "passed" | "failed",
+    type: keyof typeof selectedOptions | string
+  ) => {
+    setSelectedOptions((prev) => ({
+      ...prev,
+      [type]: selectedOptions,
+    }));
+  };
+
+  const filteredDescriptionData = useMemo(() => {
+    return descriptionData
+      .map((item: DescriptionData) => {
+        const filteredTests = item.data
+          .map((test: TestData) => {
+            const filteredMethods = test.data.filter((method: MethodData) => {
+              const appMatch =
+                !selectedOptions.app.length ||
+                selectedOptions.app.includes(item.description);
+              const browserMatch =
+                !selectedOptions.browser.length ||
+                selectedOptions.browser.includes(
+                  `${method.instanceData.browser?.name}-${method.instanceData.browser?.version}` ||
+                    ""
+                );
+              const deviceMatch =
+                !selectedOptions.device.length ||
+                selectedOptions.device.includes(
+                  method.instanceData.deviceName || ""
+                );
+              const platformMatch =
+                !selectedOptions.platform.length ||
+                selectedOptions.platform.includes(
+                  `${method.instanceData.platform.name}-${method.instanceData.platform.version}` ||
+                    ""
+                );
+              const statusMatch =
+                selectedOptions.status === "all" ||
+                (selectedOptions.status === "passed" &&
+                  parseFloat(method.misMatchPercentage) === 0) ||
+                (selectedOptions.status === "failed" &&
+                  parseFloat(method.misMatchPercentage) > 0);
+
+              return (
+                appMatch &&
+                browserMatch &&
+                deviceMatch &&
+                platformMatch &&
+                statusMatch
+              );
+            });
+
+            return { ...test, data: filteredMethods };
+          })
+          .filter((test) => test.data.length > 0);
+
+        return { ...item, data: filteredTests };
+      })
+      .filter((item) => item.data.length > 0);
+  }, [descriptionData, selectedOptions]);
 
   return (
     <div className={styles.main}>
       <h1>WebdriverIO Visual Report</h1>
       {error && <p className={styles.error}>{error}</p>}
+      <SelectHeader
+        handleSelectOptions={handleSelectOptions}
+        instanceData={instanceData as SnapshotInstanceData}
+      />
       <div>
-        {descriptionData.map((item, index) => (
+        {filteredDescriptionData.map((item, index) => (
           <Description
             key={index}
             description={item.description}
