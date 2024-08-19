b05edf2f9ba7303f47e0371f000f0a12f42ce2be
wswebcreation <wim@wswebcreation.nl>
2024-08-19 09:17:18 +0200

    chore: remove the export

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: process.env.NEXT_EXPORT === "true" ? "export" : undefined,
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: "/tmp/:path*",
-  //       destination: "/.tmp/:path*",
-  //     },
-  //   ];
-  // },
+  async rewrites() {
+    return [
+      {
+        source: "/tmp/:path*",
+        destination: "/.tmp/:path*",
+      },
+    ];
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.(png|jpe?g|gif|svg)$/i,
