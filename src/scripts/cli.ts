02093de4c1233a8ebf20bb6b40bf41e966354840
wswebcreation <wim@wswebcreation.nl>
2024-08-21 10:33:01 +0200

    feat: add output.json validation

--- src/scripts/cli.ts
+++ src/scripts/cli.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import { confirm, input, select } from "@inquirer/prompts";
-import { existsSync, mkdirSync } from "node:fs";
+import { existsSync, mkdirSync, readFileSync } from "node:fs";
 import { dirname, join, resolve } from "node:path";
 import { execSync } from "child_process";
 import { fileURLToPath } from "node:url";
@@ -13,20 +13,28 @@ import {
   runNpmScript,
 } from "./utils/cliUtils.js";
 import { CONFIG_HELPER_INTRO } from "./utils/constants.js";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-const nextBinary = resolve(__dirname, "..", "node_modules/next/dist/bin/next");
-const visualReporterProjectRoot = resolve(__dirname, "..");
-const currentPath = process.cwd();
+import { validateOutputJson } from "./utils/validateOutput.js";
 
 async function main() {
-  let filePath: string;
+  //
+  // Set some initial variables
+  let filePath: string = "";
   let reportPath: string;
   let DEBUG_MODE = false;
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const nextBinary = resolve(
+    __dirname,
+    "..",
+    "node_modules/next/dist/bin/next"
+  );
+  const visualReporterProjectRoot = resolve(__dirname, "..");
+  const currentPath = process.cwd();
 
   console.log(CONFIG_HELPER_INTRO);
 
+  //
+  // Get the output.json file path
   const initialChoice = await select<{ method: "explore" | "type" }>({
     message: "How would you like to specify the file?",
     choices: [
@@ -35,14 +43,34 @@ async function main() {
     ],
   });
 
-  if (initialChoice.method === "explore") {
-    filePath = await chooseItems({ currentPath, includeFiles: true });
-  } else {
-    filePath = await input({
-      message: "Please enter the file path:",
-    });
+  let isValidFile = false;
+  while (!isValidFile) {
+    if (initialChoice.method === "explore") {
+      filePath = await chooseItems({ currentPath, includeFiles: true });
+    } else {
+      filePath = await input({
+        message: "Please enter the file path:",
+      });
+    }
+
+    try {
+      const fileContent = JSON.parse(readFileSync(filePath, "utf8"));
+      if (validateOutputJson(fileContent)) {
+        isValidFile = true;
+      } else {
+        console.error(
+          "❌ The selected file is not valid. Please select a correct output.json file."
+        );
+      }
+    } catch (error) {
+      console.log(
+        "Failed to read or parse the file. Please select a correct output.json file."
+      );
+    }
   }
 
+  //
+  // Choose the report output folder
   const reportFolderChoice = await select<{ method: "explore" | "type" }>({
     message: "Where do you want the Visual Report to be created?",
     choices: [
@@ -58,6 +86,9 @@ async function main() {
       message: "Please enter the file path:",
     });
   }
+
+  //
+  // Check if the user wants to run in debug mode
   const runInDebugMode = await confirm({
     message: "Would you like to run in debug mode?",
   });
@@ -67,6 +98,8 @@ async function main() {
     DEBUG_MODE = true;
   }
 
+  //
+  // Build and copy the report to the specified folder
   const buildReportSpinner = ora("Building the report...\n").start();
   const reporterPath = join(reportPath, "report");
   try {
@@ -100,6 +133,8 @@ async function main() {
     throw error;
   }
 
+  //
+  // Check if the user wants to start the server and if so, start the server on the specified port
   const startServer = await confirm({
     message: "Would you like to start the server to show the report?",
   });
